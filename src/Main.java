842cdf9370c5a1ec67b1fe155cc6af4fe6317d7b
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-13 11:45:04 +0500

    Busi homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+
+            int n;
+            System.out.println("Привет введи количество цветов,а я скажу,сколько нужно вытащить, чтобы попались две с одинаковым цветом (магия!)");
+            n = scanner.nextInt();
+            System.out.println("Тебе нужно вытащить:" + (n + 1) + " бусин");
+        } catch (Exception ex) {
+            System.out.println("Ты что то не так ввёл");
+        }
     }
 }
